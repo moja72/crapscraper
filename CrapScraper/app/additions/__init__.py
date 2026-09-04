38e6ef448e4c601666bcf6c741098217916125b2
Rogério Mendes <128273138+moja72@users.noreply.github.com>
2026-09-03 22:48:57 -0300

    feat: install addition queue source filters

--- CrapScraper/app/additions/__init__.py
+++ CrapScraper/app/additions/__init__.py
@@ -1,2 +1,6 @@
 from app.additions.service import AdditionService
+from app.additions.source_filter_runtime import install_addition_source_filters
+
+install_addition_source_filters()
+
 __all__=["AdditionService"]
